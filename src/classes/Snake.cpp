455b78a2d7207f0d0ab15df20d3145caa76e3e0f
Le-Nedelec <ALex.lenedelec@gmail.Com>
2020-03-04 13:39:03 +0100

    Ajout des commentaires dans Snake.cpp

--- src/classes/Snake.cpp
+++ src/classes/Snake.cpp
@@ -3,41 +3,50 @@
 #include "Snake.h"
 #include <iostream>
 
+//Base constructeur
 Snake::Snake(){
 
     
 
 }
 
+//Accesseur du tableau serpent
 Point Snake::getSnake() const {
 
     return this->m_serpent[20];
 
 }
 
+//Accesseur de l'index
 int Snake::getIndex() const {
 
     return this->m_index;
 
 }
 
+//setter de l'index (endroit d'ajout de point dans serpent)
 void Snake::setIndex(int x){
 
     this->m_index = x ;    
 
 }
 
-// 
+/*
+ * Affiche le serpent
+ */
 void afficherSnake(){
 
 }
 
-//
+/*
+ * Ajoute un point au corp du serpent
+ */
 void ajouterPoint(Point p){
         
 
 }
 
+//Basic destructeur
 Snake::~Snake(){
 
 }
